730a71c08703b2337b0735d46a0bb9cc8855ff71
Megan Lake <meganlake95@gmail.com>
2023-11-16 18:09:30 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
+    let searchInput = useRef('')
+
+    const API_URL = 'https://itunes.apple.com/search?term='
+    
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        // Fetch Data
+        const fetchData = async () => {
+            document.title = `${term} Music`
+            const response = await fetch(API_URL + term)
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+                // Set State and Context value
+                return setData(resData.results)
+            } else {
+                return setMessage('Not Found')
+            }
+        }
+        fetchData()
+    }
+    
+    return (
+        <div>
+        {message}
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <SearchBar handleSearch = {handleSearch}/>
+                            <Gallery data={data} />
+                        </>
+                    } />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+    
+    
 }
 
 export default App;
